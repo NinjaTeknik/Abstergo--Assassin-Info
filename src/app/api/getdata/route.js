0264ca6c6f9be9e0b4714e7c6ec3ea54155f4876
Rasmus Thrane <rasmus.thrane@hotmail.com>
2024-01-18 09:57:06 +0100

    First Commit

---
+++ src/app/api/getdata/route.js
@@ -0,0 +1,22 @@
+import mysql from "mysql2/promise"
+
+const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'sample-db',
+    password: 'root',
+  });
+
+export async function POST(request) {
+    const body = await request.json()
+    console.log(body)
+    const sql = 'SELECT name, phone, address, gender FROM users WHERE id='+body.ID+' LIMIT 1'
+
+
+    const [rows, fields] = await connection.query({
+        sql,
+        rowsAsArray: false,
+        // ... other options
+      });
+    return Response.json({ rows })
+}
